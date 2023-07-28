0fed94111b2f455bb9a43945464a988ca452a781
KDrip34 <124319502+KDrip34@users.noreply.github.com>
2023-07-28 15:43:13 -0700

    Create bigballer.py

---
+++ bigballer.py
@@ -0,0 +1,6 @@
+dinnert = input("Hello Sir Big Baller as it seems,how many people are you going to treat out today on this fine evening:")
+dinnert = int(dinnert)
+if dinnert <= 30:
+	print("Man o man those cooks in back go be on overtime today lets they can be properly compensated for their efforts")
+else: 
+	print("Lets not kill this cooks tonight and I no table would be availiable for that size")
